fbd06315d88476b65b6df41907e09abc4723ce3a
karl <kaiquesilva49@hotmail.com>
2020-12-12 11:43:37 -0300

    start project

---
+++ src/main/java/com/kaique/cursospringudemy/resources/CategoriaResource.java
@@ -0,0 +1,28 @@
+package com.kaique.cursospringudemy.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kaique.cursospringudemy.domain.Categoria;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+	
+	@GetMapping
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1,"informatica");
+		Categoria cat2 = new Categoria(2,"escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
+	}
+	
+}
---
+++ src/test/java/com/kaique/cursospringudemy/CursoSpringUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaique.cursospringudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
