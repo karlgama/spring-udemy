9c4f2037bd3bfbada51544b1d9d131c9639ae746
karl <kaiquesilva49@hotmail.com>
2020-12-26 15:35:36 -0300

    Associação categoria e produto many to many

--- src/main/java/com/kaique/cursospringudemy/CursoSpringUdemyApplication.java
+++ src/main/java/com/kaique/cursospringudemy/CursoSpringUdemyApplication.java
@@ -1,13 +1,52 @@
 package com.kaique.cursospringudemy;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.kaique.cursospringudemy.domain.Categoria;
+import com.kaique.cursospringudemy.domain.Produto;
+import com.kaique.cursospringudemy.repositories.CategoriaRepository;
+import com.kaique.cursospringudemy.repositories.ProdutoRepository;
+// implementando commandLineRunner apenas para instanciar objetos e facilitar teste
+//em um ambiente de producao não é necessário
 @SpringBootApplication
-public class CursoSpringUdemyApplication {
+public class CursoSpringUdemyApplication implements CommandLineRunner{
 
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringUdemyApplication.class, args);
 	}
+	
+	//método criado apenas para facilitar teste
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		Produto p1 = new Produto(null, "computados", 2000.00);
+		Produto p2 = new Produto(null, "impressora", 800.00);
+		Produto p3 = new Produto(null, "mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
 
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+	}
+
+	
 }
